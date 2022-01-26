d981fa65c5ba86631f06fb78430591b95ba6c794
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-01-26 19:31:49 +0200

    Add files via upload

---
+++ sysanalzyer2022.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "PC TECH GREU SysAnalyzer2022" -ForegroundColor Green
+$pro_arry = @()
+$processes = Get-Process | Select Name, Id, CPU, SI
+Foreach ($process in $processes){
+$pro_arry += New-Object PSObject -Property @{
+Process_Name = $process.Names
+Process_CPU = $process.CPUs
+Process_Id = $process.Id
+Process_SI = $process.SI
+}
+}
+$op = $pro_arry | Out-GridView -Title "Grid with Filters" -OutputMode Multiple
