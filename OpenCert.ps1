a0cefce0aa42b053b6693c1a65ca5323191aba45
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-03-02 19:39:47 +0200

    Add files via upload

---
+++ OpenCert.ps1
@@ -0,0 +1,7 @@
+﻿$Computername = "user"
+
+  $CertStore = New-Object System.Security.Cryptography.X509Certificates.X509Store  -ArgumentList  "\\$($Computername)\My", "LocalMachine"
+
+  $CertStore.Open('ReadOnly')
+
+$certificate  = $CertStore.Certificates[0]
