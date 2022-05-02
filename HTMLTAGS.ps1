c95252eb3a53972633b28b7329d90f0cdc05869d
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-05-02 19:32:10 +0300

    Add files via upload

---
+++ HTMLTAGS.ps1
@@ -0,0 +1,22 @@
+﻿@{
+    AliasesToExport        = @('ConvertFrom-HTMLTag', 'ConvertFrom-HTMLClass', 'Format-JS')
+    Author                 = 'BILLMAN'
+    CmdletsToExport        = @()
+    CompanyName            = 'SuperStarPCTECHGREUUA'
+    CompatiblePSEditions   = @('Desktop', 'Core')
+    Copyright              = '(c) BILLMAN.'
+    Description            = 'Module that allows to manipulate, parse, format and optimize HTML, JavaScript and CSS'
+    DotNetFrameworkVersion = '4.7.2'
+    FunctionsToExport      = @('Convert-HTMLToText', 'ConvertFrom-HTML', 'ConvertFrom-HTMLAttributes', 'ConvertFrom-HtmlTable', 'Format-CSS', 'Format-HTML', 'Format-JavaScript', 'Optimize-CSS', 'Optimize-HTML', 'Optimize-JavaScript')
+    GUID                   = 'f0387960-7034-4918-a1e1-d5847cbf90df'
+    ModuleVersion          = '2022'
+    PowerShellVersion      = '5.1'
+    }
+    RootModule             = 'PSParseHTML.psm1'
+    ScriptsToProcess       = @()
+}
+# SIG # Begin signature block
+# MIIhjgYJKoZIhvcNAQcCoIIhfzCCIXsCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# 7Ri54FMVCyVSFkYGGqLSDQQ88MhVCE1D55vwFxVVnyjK3f1U+UQW2VNOPZchOFOM
+# eWs=
+# SIG # End signature block
