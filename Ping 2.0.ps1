b267c33157d4296cec8737dd7f81392bf6b889d7
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-05-16 19:36:28 +0300

    Add files via upload

---
+++ Ping 2.0.ps1
@@ -0,0 +1,24 @@
+﻿Test-Connection
+[-TargetName] <string[]>
+[-Ping] [-IPv4] [-IPv6] [-ResolveDestination] [-Source <string>] [-MaxHops <int>] [-Count <int>] [-Delay <int>] [-BufferSize <int>] [-DontFragment] [-TimeoutSeconds <int>] [-Quiet] [<CommonParameters>]
+
+Test-Connection
+[-TargetName] <string[]>
+-Repeat
+[-Ping] [-IPv4] [-IPv6] [-ResolveDestination] [-Source <string>] [-MaxHops <int>] [-Delay <int>] [-BufferSize <int>] [-DontFragment] [-TimeoutSeconds <int>] [-Quiet] [<CommonParameters>]
+
+Test-Connection
+[-TargetName] <string[]>
+-MtuSize
+[-IPv4] [-IPv6] [-ResolveDestination] [-TimeoutSeconds <int>] [-Quiet] [<CommonParameters>]
+
+Test-Connection
+[-TargetName] <string[]>
+-Traceroute
+[-IPv4] [-IPv6] [-ResolveDestination] [-Source <string>] [-MaxHops <int>] [-TimeoutSeconds <int>] [-Quiet] [<CommonParameters>]
+
+Test-Connection
+[-TargetName] <string[]>
+-TcpPort <int>
+[-IPv4] [-IPv6] [-ResolveDestination] [-Source <string>] [-TimeoutSeconds <int>] [-Quiet] [<CommonParameters>]
+
---
+++ Priter2.0.ps1
@@ -0,0 +1,48 @@
+﻿# Select printer dialog
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object -TypeName System.Windows.Forms.Form
+$form.Text = 'Select a printer'
+$form.Size = '400, 200'
+
+$list = @( ( Get-Printer ).Name )
+$Script:selectedvalue = ''
+
+$dropdown = New-Object -TypeName System.Windows.Forms.ComboBox
+$dropdown.Items.AddRange( $list )
+$dropdown.SelectedIndex = 0
+$dropdown.Width = '200'
+$dropdown.Location = New-Object System.Drawing.Point( [int]( ( $form.Width - $dropdown.Width ) / 2 ), 25 )
+$form.Controls.Add( $dropdown )
+
+$buttonOK_Click = {
+	$Script:selectedvalue = $dropdown.Text
+	$form.DialogResult = 'OK'
+	$form.Close( )
+}
+
+$buttonOK = New-Object System.Windows.Forms.Button
+$buttonOK.Text = 'OK'
+$buttonOK.Location = New-Object System.Drawing.Point( [int]( ( $form.Width / 2 ) - 5 - $buttonOK.Width ), 100 )
+$buttonOK.Add_Click( $buttonOK_Click )
+$form.Controls.Add( $buttonOK )
+$form.AcceptButton = $buttonOK # pressing Enter assuming OK
+
+$buttonCancel_Click = {
+	$form.DialogResult = 'Cancel'
+	$form.Close( )
+}
+
+$buttonCancel = New-Object System.Windows.Forms.Button
+$buttonCancel.Text = 'Cancel'
+$buttonCancel.Location = New-Object System.Drawing.Point( [int]( ( $form.Width / 2 ) + 5 ), 100 )
+$buttonCancel.Add_Click( $buttonCancel_Click )
+$form.Controls.Add( $buttonCancel )
+$form.CancelButton = $buttonCancel # pressing Esc assuming Cancel
+
+$result = $form.ShowDialog( )
+
+if ( $result -eq 'OK' ) {
+	$Script:selectedvalue
+}
