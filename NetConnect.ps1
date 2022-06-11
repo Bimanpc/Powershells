781d70d286ac79a7a7282510e6a5f0fd3b15984f
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-06-11 19:38:08 +0300

    Add files via upload

---
+++ NetConnect.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Welcome to Test-netconnection" -ForegroundColor Green
+Write-Host "Runnss the command with informational level"
+$infolevel=Read-Host "specify the information level"
+Write-Host "The speified info level is" $infolevel -ForegroundColor Green
+Test-NetConnection -InformationLevel $infolevel
+$infolevel=Read-Host "specify the info level"
+Write-Host "The speified info level is" $infolevel -ForegroundColor Green
+Test-NetConnection -InformationLevel $infolevel
---
+++ Printers.ps1
@@ -0,0 +1,9 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+$prntdlg = New-Object System.Windows.Forms.PrintDialog
+$prntdlg.AllowCurrentPage = $false
+$prntdlg.AllowPrintToFile = $trur
+$prntdlg.AllowSelection = $false
+$prntdlg.AllowSomePages = $true
+$prntdlg.ShowNetwork = $true
+$response = $prntdlg.ShowDialog( )# $response can return OK or Cancel
+if ( $response -eq 'OK!!' ) { Write-Host 'Selected Printer:' $prntdlg.PrinterSettings.PrinterName }
