d30cbe68f115aee28c0af093ae71a0f4cda652d6
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-07-02 19:26:02 +0300

    Add files via upload

---
+++ BIOS.ps1
@@ -0,0 +1,3 @@
+﻿Write-Host "Welcome to the script of fetching computer Info EnEx IS Fear!!"
+Write-host "The BIOS Details are this"
+Get-CimInstance -ClassName Win32_BIOS
---
+++ difference between two dates.ps1
@@ -0,0 +1,4 @@
+﻿$dateA= Get-Date
+$dateB= Get-Date "1/1/1968"
+$difference= $dateA- $dateB
+$difference|Format-List
