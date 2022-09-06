9d7c07cc27fc5092621c40620561cd7be293ce1b
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-09-06 19:36:26 +0300

    Add files via upload

---
+++ SSD Model.ps1
@@ -0,0 +1,7 @@
+﻿#Ask for SSD Model 
+[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+
+$title = 'Enter SSD Model '
+$msg   = 'Enter the SSD Model] of the Item:'
+
+$upc = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
