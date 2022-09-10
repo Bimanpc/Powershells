3cba4dfdcbceb55095f570b43b6b179e3c3bd1b8
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-09-10 19:41:34 +0300

    Add files via upload

---
+++ Loc2.0.ps1
@@ -0,0 +1,28 @@
+﻿Get-Location Syntax:
+Get-Location
+[-PSProvider <String[]>] [-PSDrive <String[]>] [<CommonParameters>]
+
+Get-Location
+[-Stack] [-StackName <String[]>] [<CommonParameters>]
+
+Set-Location Syntax:
+Set-Location
+[[-Path] <String>] [-PassThru] [<CommonParameters>]
+
+Set-Location
+-LiteralPath <String>
+[-PassThru] [<CommonParameters>]
+
+Set-Location
+[-PassThru] [-StackName <String>] [<CommonParameters>]
+
+Push-Location Syntax:
+Push-Location
+[[-Path] <String>] [-PassThru] [-StackName <String>] [<CommonParameters>]
+
+Push-Location
+[-LiteralPath <String>] [-PassThru] [-StackName <String>] [<CommonParameters>]
+
+Pop-Location Syntax:
+Pop-Location
+[-PassThru] [-StackName <String>] [<CommonParameters>]
