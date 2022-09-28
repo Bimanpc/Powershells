e3e36eee2f65465e8e44a0758e22eeeb4fb434af
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-09-28 19:37:18 +0300

    Add files via upload

---
+++ GeekitPS.ps1
@@ -0,0 +1,23 @@
+﻿$msgBoxInput =  [System.Windows.MessageBox]::Show('Do you like to GeekIT PC!!?','Game  input','YesNoCancel','Error')
+
+  switch  ($msgBoxInput) {
+
+  'Yes' {
+
+  ## Do something 
+
+  }
+
+  'No' {
+
+  ## Do something
+
+  }
+
+  'Cancel' {
+
+  ## Do something
+
+  }
+
+  }
