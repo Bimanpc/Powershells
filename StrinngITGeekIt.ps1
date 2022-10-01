d02f66ff69fbbc6d40910cbe073e28e07f94e057
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-10-01 19:40:07 +0300

    Add files via upload

---
+++ StrinngITGeekIt.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Vasiliy write host cmdlet" -ForegroundColor Green
+$test= Get-Process
+Write-Host "Print a variable" -ForegroundColor Green
+Write-Output $test
+Write-Host "Passing output to another cmdlet through pipeline" -ForegroundColor Green
+Write-Output "GeekITt" | Get-Member
+Write-Host "Vasiya enumerate and no enumerate" -ForegroundColor Green
+Write-Output 10,20,30 | Measure-Object
+Write-Output 10,20,30 -NoEnumerate | Measure-Object
