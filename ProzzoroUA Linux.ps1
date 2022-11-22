e344f12f9fb428e050e6ac80e0e203ad451c28c2
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-11-22 19:37:48 +0200

    Add files via upload

---
+++ ProzzoroUA Linux.ps1
@@ -0,0 +1,12 @@
+﻿$Question = $host.ui.PromptForChoice(
+    "Window Title", "Prozzorro is written in open source code?",(
+        [System.Management.Automation.Host.ChoiceDescription[]](
+            (New-Object System.Management.Automation.Host.ChoiceDescription "&YES","Yess"),
+            (New-Object System.Management.Automation.Host.ChoiceDescription "&No","No")
+        )
+    ), 0
+) 
+switch($Question){
+    0 {Write-Host "Yess"}
+    1 {Write-Host "Noo"}
+}
---
+++ TaskManager.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "Dispalying out grid in a GUI with search" -ForegroundColor Green
+$pro_arry = @()
+$processes = Get-Process | Select Name, Id, CPU, SI
+Foreach ($process in $processes){
+$pro_arry += New-Object PSObject -Property @{
+Process_Name = $process.Nameofservice
+Process_CPU = $process.CPUs
+Process_Id = $process.IDs
+Process_SI = $process.SIt
+}
+}
+$op = $pro_arry | Out-GridView -Title "Grid with Filters" -OutputMode Multiple
