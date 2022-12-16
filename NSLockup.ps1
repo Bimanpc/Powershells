624e8ed1b7f56ac3b7fd440b9f3865a10691f9d6
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-12-16 19:45:28 +0200

    Add files via upload

---
+++ NSLockup.ps1
@@ -0,0 +1,15 @@
+﻿Resolve-DnsName domain names(bing.com)
+Nslookup domain names
+Nslookup ip-address
+Nslookup -type= domain
+Get-DnsClientServerAddress
+Get-DnsClientCache
+Resolve-DnsName domain -CacheOnly
+Resolve-DnsName domain -DnsOnly
+Resolve-DnsName domain -Type MX
+Resolve-DnsName domain -Type SOA
+Resolve-DnsName domain -Type A
+Resolve-DnsName domain -Type NS
+Resolve-DnsName domain -Type CNAME
+Resolve-DnsName domain -Type TXT
+Resolve-DnsName domain -Type PTR
