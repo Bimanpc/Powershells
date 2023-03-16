3049a5452b4027026f0861f97255b793a1283252
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-03-16 19:49:41 +0200

    Add files via upload

---
+++ adduser.ps1
@@ -0,0 +1,5 @@
+﻿NAME
+Add-LocalGroupMember
+SYNTAX
+Add-LocalGroupMember [-Group] <LocalGroup> [-Member] <LocalPrincipal[]> [-WhatIf] [-Confirm]  [<CommonParameters>] Add-LocalGroupMember [-Name] <string> [-Member] <LocalPrincipal[]> [-WhatIf] [-Confirm]  [<CommonParameters>] Add-LocalGroupMember [-SID] <SecurityIdentifier> [-Member] <LocalPrincipal[]> [-WhatIf] [-Confirm]  [<CommonParameters>] ALIASES
+algm
