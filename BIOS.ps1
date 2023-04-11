23ee96943751059438b501512fb579622a89421a
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-04-11 19:46:21 +0300

    Add files via upload

--- BIOS.ps1
+++ BIOS.ps1
@@ -1,3 +1,1 @@
-﻿Write-Host "Welcome to the script of fetching computer Info EnEx IS Fear!!"
-Write-host "The BIOS Details are this"
-Get-CimInstance -ClassName Win32_BIOS
+﻿Get-CimInstance -ClassName Win32_BIOS
