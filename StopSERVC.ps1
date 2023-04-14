43a718bc107348701718594d88f905740e2347c0
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-04-14 19:43:23 +0300

    Add files via upload

---
+++ StopSERVC.ps1
@@ -0,0 +1,5 @@
+﻿Stop-Service
+[-Force] [-NoWait] [-Name] <String[]>
+[-InputObject] <ServiceController[]>
+[-DisplayName] <String[]>
+[-PassThru] [-Include <String[]>] [-Exclude <String[]>] [-WhatIf] [-Confirm] [<CommonParameters>]
