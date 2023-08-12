22fa61666b1eb785ebba965ff0f69870dfa4e9d4
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-08-12 19:41:35 +0300

    Add files via upload

---
+++ MyNetInfo.ps1
@@ -0,0 +1,33 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+# Create the form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Network Information"
+$form.Size = New-Object System.Drawing.Size(400, 300)
+
+# Create labels
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(20, 20)
+$label.Size = New-Object System.Drawing.Size(360, 20)
+$label.Text = "Network Adapters:"
+$form.Controls.Add($label)
+
+# Create a listbox to display network adapter details
+$listBox = New-Object System.Windows.Forms.ListBox
+$listBox.Location = New-Object System.Drawing.Point(20, 50)
+$listBox.Size = New-Object System.Drawing.Size(360, 200)
+$form.Controls.Add($listBox)
+
+# Get network adapter information
+$networkAdapters = Get-WmiObject Win32_NetworkAdapter | Where-Object { $_.NetConnectionStatus -eq 2 }
+
+foreach ($adapter in $networkAdapters) {
+    $listBox.Items.Add("Name: " + $adapter.Name)
+    $listBox.Items.Add("Description: " + $adapter.Description)
+    $listBox.Items.Add("MAC Address: " + $adapter.MACAddress)
+    $listBox.Items.Add("IP Address: " + ($adapter | Get-WmiObject Win32_NetworkAdapterConfiguration).IPAddress[0])
+    $listBox.Items.Add("")  # Add a blank line between adapters
+}
+
+# Show the form
+$form.ShowDialog()
