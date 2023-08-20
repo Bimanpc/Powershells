a4c72c26d024c9877ec902c7863f7b6c540f07e0
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-08-20 19:40:10 +0300

    Add files via upload

---
+++ CPU SPEC.ps1
@@ -0,0 +1,16 @@
+﻿# Load the XAML GUI
+Add-Type -AssemblyName PresentationFramework
+$XAML = [System.IO.File]::ReadAllText('CpuSpecGui.xaml')
+$Reader = (New-Object System.Xml.XmlNodeReader $XAML)
+$Window = [Windows.Markup.XamlReader]::Load($Reader)
+
+# Define the event handler for the "Get CPU Info" button
+$Button_GetCpuInfo_Click = {
+    $cpuInfo = Get-WmiObject Win32_Processor
+    $cpuInfoText.Text = "Processor: $($cpuInfo.Name)`r`nArchitecture: $($cpuInfo.Architecture)`r`nCores: $($cpuInfo.NumberOfCores)`r`nThreads: $($cpuInfo.NumberOfLogicalProcessors)"
+}
+
+$Window.FindName("Button_GetCpuInfo_Click").Add_Click($Button_GetCpuInfo_Click)
+
+# Display the GUI
+$Window.ShowDialog() | Out-Null
---
+++ NetInf.ps1
@@ -0,0 +1,18 @@
+﻿# Load the XAML GUI
+Add-Type -AssemblyName PresentationFramework
+$XAML = [System.IO.File]::ReadAllText('NetworkInfoGui.xaml')
+$Reader = (New-Object System.Xml.XmlNodeReader $XAML)
+$Window = [Windows.Markup.XamlReader]::Load($Reader)
+
+# Define the event handler for the "Get Network Info" button
+$Button_GetNetworkInfo_Click = {
+    $networkInfo = Get-NetAdapter | Select-Object Name, InterfaceDescription, MacAddress, Status, LinkSpeed
+    $networkInfoText.Text = $networkInfo | ForEach-Object {
+        "Name: $($_.Name)`r`nDescription: $($_.InterfaceDescription)`r`nMAC Address: $($_.MacAddress)`r`nStatus: $($_.Status)`r`nLink Speed: $($_.LinkSpeed) Mbps`r`n`r`n"
+    }
+}
+
+$Window.FindName("Button_GetNetworkInfo_Click").Add_Click($Button_GetNetworkInfo_Click)
+
+# Display the GUI
+$Window.ShowDialog() | Out-Null
