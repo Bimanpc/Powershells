21ca7466fdef79e201fde6639806ecafe7d89b63
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-08-26 19:41:38 +0300

    Add files via upload

---
+++ PC Benchh.ps1
@@ -0,0 +1,20 @@
+﻿# Button click event handler
+$buttonRunBenchmark_Click = {
+    $scriptPath = "C:\Path\To\Your\BenchmarkScript.ps1"
+    
+    # Run the benchmark script
+    Start-Process powershell -ArgumentList "-File $scriptPath"
+    
+    # Display a message to the user
+    $labelStatus.Text = "Benchmarking completed."
+}
+# Label to display benchmark results
+$labelResults = New-Object System.Windows.Forms.Label
+$labelResults.Location = New-Object System.Drawing.Point(10, 100)
+$labelResults.Size = New-Object System.Drawing.Size(400, 50)
+$form.Controls.Add($labelResults)
+# Label to display benchmark results
+$labelResults = New-Object System.Windows.Forms.Label
+$labelResults.Location = New-Object System.Drawing.Point(10, 100)
+$labelResults.Size = New-Object System.Drawing.Size(400, 50)
+$form.Controls.Add($labelResults)
