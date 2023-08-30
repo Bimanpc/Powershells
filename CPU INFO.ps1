7a43995217303fc0d4bd0552c036dd68dec2f702
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-08-30 19:41:22 +0300

    Add files via upload

---
+++ CPU INFO.ps1
@@ -0,0 +1,9 @@
+﻿# Get CPU information
+$cpuInfo = Get-WmiObject Win32_Processor
+
+# Display CPU information
+Write-Host "CPU Manufacturer: $($cpuInfo.Manufacturer)"
+Write-Host "CPU Model: $($cpuInfo.Name)"
+Write-Host "Number of Cores: $($cpuInfo.NumberOfCores)"
+Write-Host "Number of Logical Processors: $($cpuInfo.NumberOfLogicalProcessors)"
+Write-Host "Max Clock Speed: $($cpuInfo.MaxClockSpeed) MHz"
