03b7e6253b98ce4e64c9eba907ddb154caa0e526
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-09-10 19:33:23 +0300

    Add files via upload

---
+++ Chat.ps1
@@ -0,0 +1,41 @@
+﻿# Load the WinForms assembly
+Add-Type -AssemblyName System.Windows.Forms
+
+# Create a form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Chatbox 2.0"
+$form.Size = New-Object System.Drawing.Size(400, 300)
+
+# Create a text box to display the chat messages
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Multiline = $true
+$textBox.ReadOnly = $true
+$textBox.ScrollBars = 'Vertical'
+$textBox.Location = New-Object System.Drawing.Point(10, 10)
+$textBox.Size = New-Object System.Drawing.Size(380, 200)
+$form.Controls.Add($textBox)
+
+# Create a text box for entering messages
+$messageTextBox = New-Object System.Windows.Forms.TextBox
+$messageTextBox.Location = New-Object System.Drawing.Point(10, 220)
+$messageTextBox.Size = New-Object System.Drawing.Size(300, 30)
+$form.Controls.Add($messageTextBox)
+
+# Create a button to send messages
+$sendButton = New-Object System.Windows.Forms.Button
+$sendButton.Location = New-Object System.Drawing.Point(320, 220)
+$sendButton.Size = New-Object System.Drawing.Size(70, 30)
+$sendButton.Text = "Send"
+$sendButton.Add_Click({
+    $message = $messageTextBox.Text
+    if ($message -ne "") {
+        $textBox.AppendText("You: $message`r`n")
+        # Here, you can send the message to another user or process it as needed.
+        # For this simplified example, we just clear the message box.
+        $messageTextBox.Text = ""
+    }
+})
+$form.Controls.Add($sendButton)
+
+# Show the form
+$form.ShowDialog()
---
+++ NetToolbox.ps1
@@ -0,0 +1,38 @@
+﻿# Load the WinForms assembly
+Add-Type -AssemblyName System.Windows.Forms
+
+# Create a new form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "Net Tools"
+$form.Size = New-Object System.Drawing.Size(400, 200)
+
+# Create a label
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Point(10, 20)
+$label.Size = New-Object System.Drawing.Size(120, 20)
+$label.Text = "Enter IP Addressss:"
+$form.Controls.Add($label)
+
+# Create a text box for entering IP address
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(140, 20)
+$textBox.Size = New-Object System.Drawing.Size(200, 20)
+$form.Controls.Add($textBox)
+
+# Create a button for pinging the IP address
+$button = New-Object System.Windows.Forms.Button
+$button.Location = New-Object System.Drawing.Point(140, 50)
+$button.Size = New-Object System.Drawing.Size(100, 30)
+$button.Text = "Ping"
+$button.Add_Click({
+    $ip = $textBox.Text
+    if (Test-Connection -ComputerName $ip -Count 1 -Quiet) {
+        [System.Windows.Forms.MessageBox]::Show("Ping successful!", "Result", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+    } else {
+        [System.Windows.Forms.MessageBox]::Show("Ping failed.", "Result", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+    }
+})
+$form.Controls.Add($button)
+
+# Show the form
+$form.ShowDialog()
