7e3a3c6204ce59252315201211fb611cf51773c5
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-09-24 19:41:42 +0300

    Add files via upload

---
+++ WinformVasiya.ps1
@@ -0,0 +1,18 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "PowerShell Vasiya GUI"
+$form.Size = New-Object System.Drawing.Size(300, 200)
+$form.StartPosition = "CenterScreen"
+
+$button = New-Object System.Windows.Forms.Button
+$button.Text = "Click Me!!!"
+$button.Location = New-Object System.Drawing.Point(100, 70)
+
+$button.Add_Click({
+    [System.Windows.Forms.MessageBox]::Show("Button clicked!", "Information", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+})
+
+$form.Controls.Add($button)
+
+$form.ShowDialog()
