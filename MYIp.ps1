fc62a1b4c9fc3fc666bbf4c8477ac159905934c8
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-09-28 19:39:14 +0300

    Add files via upload

---
+++ MYIp.ps1
@@ -0,0 +1,33 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+# Create a form
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "IP Address Viewer"
+$form.Size = New-Object System.Drawing.Size(300, 100)
+$form.StartPosition = "CenterScreen"
+
+# Create a label to display the IP address
+$label = New-Object System.Windows.Forms.Label
+$label.Text = "Your IP Address:"
+$label.Location = New-Object System.Drawing.Point(10, 10)
+$label.Size = New-Object System.Drawing.Size(150, 20)
+$form.Controls.Add($label)
+
+# Create a textbox to display the IP address
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Location = New-Object System.Drawing.Point(160, 10)
+$textBox.Size = New-Object System.Drawing.Size(120, 20)
+$textBox.ReadOnly = $true
+$form.Controls.Add($textBox)
+
+# Function to get the IP address
+Function Get-IPAddress {
+    $ipAddress = (Test-Connection -ComputerName (hostname) -Count 1).IPv4Address.IPAddressToString
+    $textBox.Text = $ipAddress
+}
+
+# Call the function to get the IP address
+Get-IPAddress
+
+# Show the form
+$form.ShowDialog() | Out-Null
