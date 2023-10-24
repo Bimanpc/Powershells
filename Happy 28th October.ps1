7e6cf99a53de1cb7dfed911e049b287ba75d3be6
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-10-24 19:28:12 +0300

    Add files via upload

---
+++ Happy 28th October.ps1
@@ -0,0 +1,20 @@
+﻿Add-Type -TypeDefinition @"
+    using System;
+    using System.Windows.Forms;
+"@
+
+# Create a new form
+$form = New-Object Windows.Forms.Form
+$form.Text = "Happy 28th October"
+
+# Create a label to display the message
+$label = New-Object Windows.Forms.Label
+$label.Text = "Happy 28th October!!!!!"
+$label.AutoSize = $true
+$label.Location = New-Object Drawing.Point(50, 50)
+
+# Add the label to the form
+$form.Controls.Add($label)
+
+# Show the form
+$form.ShowDialog()
