fba085b42c707dea7b6af57509f3cbc5563bcdb3
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-11-10 19:41:18 +0200

    Add files via upload

---
+++ MP3 Info.ps1
@@ -0,0 +1,59 @@
+﻿Add-Type -Path ".\TagLibSharp.dll"
+
+# XAML for the GUI
+[xml]$xaml = @'
+<Window
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="MP3 Info" Height="300" Width="400">
+    <Grid>
+        <StackPanel Margin="10">
+            <Label Content="MP3 File:"/>
+            <TextBox x:Name="txtFilePath" Width="300" Margin="0,0,0,10"/>
+            <Button Content="Load MP3" Width="80" Height="30" Margin="0,0,0,10" Click="LoadMp3_Click"/>
+            <TextBlock x:Name="txtInfo" TextWrapping="Wrap"/>
+        </StackPanel>
+    </Grid>
+</Window>
+'@
+
+# Create XML reader
+$reader = (New-Object System.Xml.XmlNodeReader $xaml)
+$window = [Windows.Markup.XamlLoader]::Load($reader)
+
+# Define the event handler
+$window.FindName('LoadMp3_Click').Add_Click({
+    $filePath = $window.FindName('txtFilePath').Text
+    if (Test-Path $filePath -and $filePath -match '\.mp3$') {
+        # Load MP3 file information
+        $file = [TagLib.File]::Create($filePath)
+        $tag = $file.GetTag()
+
+        # Display MP3 file information in the GUI
+        $info = "Title: $($tag.Title)`n"
+        $info += "Artist: $($tag.Artists)`n"
+        $info += "Album: $($tag.Album)`n"
+        $info += "Year: $($tag.Year)`n"
+        $info += "Genre: $($tag.Genres -join ', ')`n"
+        $info += "Duration: $($file.Properties.Duration.ToString())"
+
+        $window.FindName('txtInfo').Text = $info
+    } else {
+        $window.FindName('txtInfo').Text = "Invalid or non-existent MP3 file."
+    }
+})
+
+# Show the GUI
+[Windows.Markup.XamlLoader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
+[Windows.Markup.XamlLoader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
+[Windows.Markup.XamlLoader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
+
+# Start the GUI event loop
+[Windows.Markup.XamlLoader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
+[Windows.Markup.XamlLoader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
+[Windows.Markup.XamlLoader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
+
+# Display the GUI
+[Windows.Markup.XamlLoader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
+[Windows.Markup.XamlLoader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
+[Windows.Markup.XamlLoader]::Load((New-Object System.Xml.XmlNodeReader $xaml))
---
+++ Merry XMAS.ps1
@@ -0,0 +1,18 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+# Create a form
+$form = New-Object Windows.Forms.Form
+$form.Text = "Merry Christmas"
+$form.Size = New-Object Drawing.Size @(300,150)
+
+# Create a label with the message
+$label = New-Object Windows.Forms.Label
+$label.Text = "Merry Christmas!"
+$label.AutoSize = $true
+$label.Location = New-Object Drawing.Point @(75, 30)
+
+# Add the label to the form
+$form.Controls.Add($label)
+
+# Show the form
+$form.ShowDialog()
