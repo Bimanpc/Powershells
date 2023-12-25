acc957deead518f165f69c823e0470e9b9289523
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-12-25 19:37:19 +0200

    Create Aurism.ps1

---
+++ Aurism.ps1
@@ -0,0 +1,11 @@
+# Autism Support Script
+
+Write-Host "Hello friend with autism!"
+Write-Host "Your uniqueness is truly special."
+Write-Host "You bring joy and light to those around you."
+Write-Host "Remember, you are not alone, and you are loved."
+
+# Additional information or resources about autism
+Write-Host "If you'd like to learn more about autism, visit: https://www.autismspeaks.org/"
+
+# End of script
